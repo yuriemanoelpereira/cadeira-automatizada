aec419c9a2dc4e1a221df2b134bab96887f8488f
yuriemanoelpereira <38574358+yuriemanoelpereira@users.noreply.github.com>
2018-04-20 22:43:57 -0300

    Add files via upload

---
+++ cadeira_teste_novos_conceitos.ino
@@ -0,0 +1,111 @@
+//*************** Definição dos Pinos ***********************
+#define pinIN1 10    //Motor 1
+#define pinIN2 11     //Motor 1
+#define pinIN3 6     //Motor 2
+#define pinIN4 5     //Motor 2 
+#define pinPot1  A0  //Potenciometro Frente e Reverso
+#define pinPot2  A2  //Potenciometro Direito e Esquerdo
+#define pin 8
+
+#include <Ultrasonic.h>
+
+#define TRIGGER_PIN  2
+#define ECHO_PIN     4
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+
+
+//*************** Variaveis para registro do estado do Joystick  ************
+int pot1 = 512;
+int pot2 = 512;
+boolean botao1 = false;
+
+
+//*************** Variaveis para controle da direção ************
+int pDireita  = 100;
+int pEsquerda = 100;
+
+
+void setup() {
+  pinMode(pin, OUTPUT);
+  pinMode(pinIN1, OUTPUT);
+  pinMode(pinIN2, OUTPUT);
+  pinMode(pinIN3, OUTPUT);
+  pinMode(pinIN4, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+
+  //Leitura do Joystick (potenciometros)
+  pot1 = analogRead(pinPot1);
+  pot2 = analogRead(pinPot2);
+
+
+  //Controle da direção
+  if (pot2 < 512) {
+    //Esquerda
+    pDireita  = 100;
+    pEsquerda = map(pot2, 511, 0, 100, 0);
+  } else {
+    //Direita
+    pDireita  = map(pot2, 512, 1023, 100, 0);
+    pEsquerda = 100;
+  }
+
+  if (pot1 < 512) {
+    //Reverso
+    int velocidade = map(pot1, 511, 0, 0, 255);
+
+    analogWrite(pinIN1, 0);
+    analogWrite(pinIN2, velocidade * pDireita / 100);
+
+    analogWrite(pinIN3, 0);
+    analogWrite(pinIN4, velocidade * pEsquerda / 100);
+
+  }
+
+
+  else {
+    //Para frente
+    int velocidade = map(pot1, 512, 1023, 0, 255);
+
+    analogWrite(pinIN1, velocidade * pDireita / 100);
+    analogWrite(pinIN2, 0);
+
+    analogWrite(pinIN3, velocidade * pEsquerda / 100);
+    analogWrite(pinIN4, 0);
+  }
+
+
+    if(cmMsec < 100 && pot1 < 50){
+
+
+    digitalWrite(8, HIGH);
+
+  }
+
+  else {
+
+    digitalWrite(8, LOW);
+
+  }
+
+
+
+  Serial.print(", /n CM: ");
+  Serial.print(cmMsec);
+  
+}
+
+
+
+
+
---
+++ projeto_cadeira.ino
@@ -0,0 +1,131 @@
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+
+  //Definindo os pinos para o trigger e echo
+#define TRIGGER_PIN  2
+#define ECHO_PIN     4
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+unsigned int eixoY;
+unsigned int eixoX;
+unsigned int pwm_y;
+unsigned int pwm_x;
+
+void setup()
+{
+  //iniciando a porta serial
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+
+  //Le as informacoes do sensor, em cm
+  float cmMsec;
+  long microsec = ultrasonic.timing();
+
+   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+   
+  //inicia os pinos do joystick
+  eixoY = analogRead(A2);
+  eixoX = analogRead(A0);
+
+  pwm_y = map(eixoY, 0, 1023, 0, 255);
+  pwm_x = map(eixoX, 0, 1023, 0, 255);
+
+  //cadeira vai para frente
+  if (pwm_y > 140) {
+
+    analogWrite(10, pwm_y);
+    analogWrite(11, 0);
+    analogWrite(5, pwm_y - 8);
+    analogWrite(6, 0);
+  }
+
+  //cadeira vai para atras
+  else if (pwm_y < 50) {
+
+    analogWrite(11, 255 - pwm_y);
+    analogWrite(10, 0);
+    analogWrite(6, 247 - pwm_y);
+    analogWrite(5, 0);
+  }
+
+
+
+  //cadeira se move no sentido horário
+  else if (pwm_x > 240) {
+
+    analogWrite(11, 0);
+    analogWrite(10, pwm_x - 25);
+    analogWrite(6, 0);
+    analogWrite(5, 0);
+
+  }
+
+  //cadeira se move no sentido anti-horário
+
+  else if (pwm_x < 30) {
+
+    analogWrite(11, 0);
+    analogWrite(10, 0);
+    analogWrite(5, 230 - pwm_x);
+    analogWrite(6, 0);
+
+  }
+
+  //posição de repouso
+  else {
+
+    analogWrite(10, 0);
+    analogWrite(11, 0);
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+  }
+
+
+  // paramentros para o emisor sonoro ativar
+
+  if (cmMsec < 100 && pwm_y < 10) {
+
+    digitalWrite(8, HIGH);
+
+  }
+
+  else if (cmMsec < 100 && pwm_x > 200) {
+
+
+     digitalWrite(8, HIGH);
+
+  }
+
+
+  else if (cmMsec < 100 && pwm_x < 10) {
+
+
+    digitalWrite(8, HIGH);
+
+  }
+
+  else {
+
+    digitalWrite(8, LOW);
+
+  }
+
+
+  //Exibe informacoes no serial monitor
+  Serial.print("\n Distancia em cm:  ");
+  Serial.print(cmMsec);
+
+
+
+}
